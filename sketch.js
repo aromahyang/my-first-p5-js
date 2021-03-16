1d2b83f65d384aff1204012e0e9c164aa2fe2517
Jihyang Lee <haeng1605@gmail.com>
2021-03-17 00:05:28 +0900

    드래그한 선 렌더링하기

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
+let previous;
+let current;
+let isDragging = false;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(255);
+	previous = createVector(0, 0);
+	current = createVector(0, 0);
 }
 
 function draw() {
+	background(255);
 
+	if (isDragging) {
+		stroke("#FAD4AE");
+		strokeWeight(10);
+		line(previous.x, previous.y, mouseX, mouseY);
+	}
+}
+
+function mousePressed() {
+	isDragging = true;
+	previous.x = mouseX;
+	previous.y = mouseY;
+}
+
+function mouseDragged() {
+	current.x = mouseX;
+	current.y = mouseY;
+}
+
+function mouseReleased() {
+	isDragging = false;
 }
