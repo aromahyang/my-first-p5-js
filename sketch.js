c20b626f5424220261ab88cf9c33b5ade5380ee9
Jihyang Lee <haeng1605@gmail.com>
2021-04-06 22:54:41 +0900

    여러 공 발사하기

--- sketch.js
+++ sketch.js
@@ -1,15 +1,52 @@
+const NUMBER_OF_BALLS = 10;
+const balls = [];
+
 let previous;
 let current;
 let isDragging = false;
 let isShooting = false;
-let slope;
-let speed;
-let radian;
+
+class Ball {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.d = 30;
+		this.color = `rgb(${floor(random(255))},${floor(random(255))},${floor(random(255))})`;
+		this.speed = 0;
+		this.degree = 0;
+	}
+
+	setProperties(x, y, s, d) {
+		this.x = x;
+		this.y = y;
+		this.speed = s;
+		this.degree = d;
+	}
+
+	updatePosition() {
+		const radian = Math.atan2((current.y - previous.y), (current.x - previous.x));
+		const newRadian = radian + (this.degree * Math.PI / 180);
+		const sine = Math.sin(newRadian);
+		const cosine = Math.cos(newRadian);
+
+		this.x = this.x - this.speed * cosine;
+		this.y = this.y - this.speed * sine;
+	}
+
+	render() {
+		fill(this.color);
+		noStroke();
+		circle(this.x, this.y, this.d);
+	}
+}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	previous = createVector(0, 0);
 	current = createVector(0, 0);
+	for(let i = 0 ; i < NUMBER_OF_BALLS ; i++) {
+		balls[i] = new Ball(0, 0);
+	}
 }
 
 function draw() {
@@ -22,17 +59,17 @@ function draw() {
 
 		const dx = current.x - previous.x;
 		const dy = current.y - previous.y;
-		radian = Math.atan2(dy, dx);
-		slope = dy / dx;
-		speed = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)) / 10;
+		const speed = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)) / 10;
+		for(let i = 0 ; i < NUMBER_OF_BALLS ; i++) {
+			balls[i].setProperties(current.x, current.y, speed, random(-5, 5));
+		}
 	}
 
 	if (isShooting) {
-		noStroke();
-		fill('#173F5F');
-		circle(current.x, current.y, 30);
-		current.x = current.x - speed * Math.cos(radian);
-		current.y = current.y - speed * Math.sin(radian);
+		for(let i = 0 ; i < NUMBER_OF_BALLS ; i++) {
+			balls[i].render();
+			balls[i].updatePosition();
+		}
 	}
 }
 
